c750fedc7803accb43284c286a83a7dcf07bc7ab
vinimadu <vinicius.madu@gmail.com>
2025-03-29 16:15:30 -0300

    add files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# fo_zoo_models
+# Custom Faster RCNN
+
+Wrapper for Faster RCNN model for the FiftyOne Model Zoo.
+
+This model was trained on the [German Traffic Sign Detection Benchmark](https://benchmark.ini.rub.de/gtsdb_dataset.html) with a modification to detect a single class: 'traffic sign'.
+
+You can follow the tutorial in [fiftyone-pytorch_detection_training](https://github.com/voxel51/fiftyone-examples/blob/master/examples/pytorch_detection_training.ipynb) and the files provided in this repository to train your own custom Faster RCNN and add it to the FiftyOne Zoo. 
+
+For that, you may need to adapt the following fields in `manifest.json`:
+- `"base_name"`: Set this to a descriptive name for your model, e.g., `"faster-rcnn-gtsdb"`.
+- `"base_filename"`: Specify the filename of your model, e.g., `"faster_rcnn_gtsdb.pt"`.
+- `"google_drive_id"`: Provide the Google Drive ID where your model is hosted.
+
+## Example Usage
+
+``` python
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+dataset = foz.load_zoo_dataset(
+    "coco-2017",
+    split="validation",
+    max_samples=50,
+    shuffle=True,
+)
+
+foz.register_zoo_model_source("https://github.com/vinimadu/custom-fasterrcnn")
+
+model = foz.load_zoo_model("faster-rcnn-gtsdb-single-class")
+dataset.apply_model(model, label_field="predictions")
+```
---
+++ __init__.py
@@ -0,0 +1,45 @@
+import torch
+import fiftyone.utils.torch as fout
+
+def download_model(model_name, model_path):
+    pass
+
+def load_model(model_name, model_path, classes, **kwargs):
+    """Loads the model.
+
+    Args:
+        model_name: the name of the model to load, as declared by the
+            ``base_name`` and optional ``version`` fields of the manifest
+        model_path: the absolute filename or directory to which the model was
+            donwloaded, as declared by the ``base_filename`` field of the
+            manifest
+        **kwargs: optional keyword arguments that configure how the model
+            is loaded
+
+    Returns:
+        a :class:`fiftyone.core.models.Model`
+    """
+
+    # Consturct the specified `Model` instance, generally by importing
+    # other modules in `model_dir`
+
+    d = dict(model_path=model_path, classes=classes)
+
+    config = CustomFrcnnModelConfig(d)
+
+    return CustomFrcnnModel(config)
+
+class CustomFrcnnModelConfig(fout.TorchImageModelConfig):
+    def __init__(self, d):
+        super().__init__(d)
+
+        self.model_path = self.parse_string(d, "model_path")
+
+        self.entrypoint_fcn = "torchvision.models.detection.faster_rcnn.fasterrcnn_resnet50_fpn"
+        self.output_processor_cls = "fiftyone.utils.torch.DetectorOutputProcessor"
+
+class CustomFrcnnModel(fout.TorchImageModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self._model = torch.load(config.model_path,weights_only=False)
