ab70441a82483587468045157d49800a4d8cf0ab
vinimadu <vinicius.madu@gmail.com>
2025-06-05 11:39:27 -0300

    Refactor model loading and configuration; add MobileNetV2 model to manifest

--- __init__.py
+++ __init__.py
@@ -23,23 +23,43 @@ def load_model(model_name, model_path, classes, **kwargs):
     # Consturct the specified `Model` instance, generally by importing
     # other modules in `model_dir`
 
+    config_dict = MODEL_TYPES[model_name]
+
     d = dict(model_path=model_path, classes=classes)
 
-    config = CustomFrcnnModelConfig(d)
+    config = CustomModelConfig(d, config_dict)
 
-    return CustomFrcnnModel(config)
+    return CustomModel(config)
 
-class CustomFrcnnModelConfig(fout.TorchImageModelConfig):
-    def __init__(self, d):
+class CustomModelConfig(fout.TorchImageModelConfig):
+    def __init__(self, d, config_dict):
         super().__init__(d)
 
         self.model_path = self.parse_string(d, "model_path")
 
-        self.entrypoint_fcn = "torchvision.models.detection.faster_rcnn.fasterrcnn_resnet50_fpn"
-        self.output_processor_cls = "fiftyone.utils.torch.DetectorOutputProcessor"
+        for k, v in config_dict.items():
+            setattr(self, k, v)
 
-class CustomFrcnnModel(fout.TorchImageModel):
+class CustomModel(fout.TorchImageModel):
     def __init__(self, config):
         super().__init__(config)
 
         self._model = torch.load(config.model_path,weights_only=False)
+
+MODEL_TYPES = {
+    "faster-rcnn-gtsdb-single-class": {
+        'type': 'detection',
+        'entrypoint_fcn': "torchvision.models.detection.faster_rcnn.fasterrcnn_resnet50_fpn",
+        'output_processor_cls': "fiftyone.utils.torch.DetectorOutputProcessor"
+    },
+    "mobilenet_v2-torch-day-night": {
+        'type': 'classification',
+        'entrypoint_fcn': "torchvision.models.detection.faster_rcnn.fasterrcnn_resnet50_fpn",
+        'output_processor_cls': "fiftyone.utils.torch.ClassifierOutputProcessor",
+        "image_min_dim": 224,
+        "image_max_dim": 2048,
+        "image_mean": [0.485, 0.456, 0.406],
+        "image_std": [0.229, 0.224, 0.225],
+        "embeddings_layer": "<classifier.1"
+    }
+}
