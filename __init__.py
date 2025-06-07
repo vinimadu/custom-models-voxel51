ef902d0a6f7de36c9bcf73226f726051edde143d
vinimadu <vinicius.madu@gmail.com>
2025-06-07 18:12:47 -0300

    Add ResNet-18 model configuration and update model loading to support device mapping

--- __init__.py
+++ __init__.py
@@ -44,16 +44,15 @@ class CustomModel(fout.TorchImageModel):
     def __init__(self, config):
         super().__init__(config)
 
-        self._model = torch.load(config.model_path,weights_only=False)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self._model = torch.load(config.model_path,weights_only=False,map_location=device)
 
 MODELS_CONFIG = {
     "faster-rcnn-gtsdb-single-class": {
-        'type': 'detection',
         'entrypoint_fcn': "torchvision.models.detection.faster_rcnn.fasterrcnn_resnet50_fpn",
         'output_processor_cls': "fiftyone.utils.torch.DetectorOutputProcessor"
     },
     "mobilenet_v2-torch-day-night": {
-        'type': 'classification',
         'entrypoint_fcn': "torchvision.models.mobilenet.mobilenet_v2",
         'output_processor_cls': "fiftyone.utils.torch.ClassifierOutputProcessor",
         "image_min_dim": 224,
@@ -61,5 +60,14 @@ MODELS_CONFIG = {
         "image_mean": [0.485, 0.456, 0.406],
         "image_std": [0.229, 0.224, 0.225],
         "embeddings_layer": "<classifier.1"
+    },
+    "resnet18-torch-day-night": {
+        'entrypoint_fcn': "torchvision.models.resnet.resnet18",
+        'output_processor_cls': "fiftyone.utils.torch.ClassifierOutputProcessor",
+        "image_min_dim": 224,
+        "image_max_dim": 2048,
+        "image_mean": [0.485, 0.456, 0.406],
+        "image_std": [0.229, 0.224, 0.225],
+        "embeddings_layer": "<fc"
     }
 }
